ae20d7c46eba73f55216c0046ef0ef396c0e4ed3
한주연 <juyeon.han@yaksonhealthcare.com>
2025-02-12 11:44:42 +0900

    feat: carousel 로직 디벨롭

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import Icon from '@/styles/icons/icons';
 import styles from './Carousel.module.scss';
@@ -24,54 +24,74 @@ const Carousel = forwardRef<CarouselHandle, CarouselProps>((props, ref) => {
   const cx = classNames.bind(styles);
   const { width = '100%', imgArr, subImgNum = 5, ...otherProps } = props;
 
-  const [imgIdx, setImgIdx] = useState<number>(0);
-  const showImg = imgArr[imgIdx];
+  const [showImgIdx, setShowImgIdx] = useState<number>(0);
+  const [thumbStartIdx, setThumbStartIdx] = useState<number>(0);
+
+  const showImg = imgArr[showImgIdx];
 
   const handleImgIdx = (dir: 'left' | 'right') => {
     if (dir === 'right') {
-      setImgIdx((prev) => (prev + 1) % imgArr.length);
+      setShowImgIdx((prev) => (prev + 1) % imgArr.length);
     } else {
-      setImgIdx((prev) => (prev - 1 + imgArr.length) % imgArr.length);
+      setShowImgIdx((prev) => (prev - 1 + imgArr.length) % imgArr.length);
     }
   };
 
   const handleSubImg = (idx: number) => {
-    setImgIdx(idx);
+    setShowImgIdx(idx);
   };
 
-  const thumbImgNum = imgArr.length < subImgNum ? imgArr.length : subImgNum;
+  const thumbImgLength = imgArr.length < subImgNum ? imgArr.length : subImgNum;
+
+  useEffect(() => {
+    const thumbIdxArr = Array.from(
+      { length: thumbImgLength },
+      (_, i) => (thumbStartIdx + i) % imgArr.length
+    );
+    const thumbEndIdx = thumbIdxArr[thumbIdxArr.length - 1];
+    if (!thumbIdxArr.includes(showImgIdx)) {
+      // right
+      if (showImgIdx === (thumbEndIdx + 1) % imgArr.length) {
+        setThumbStartIdx((prev) => (prev + 1) % imgArr.length);
+      }
+      //left
+      if (showImgIdx === (thumbStartIdx - 1 + imgArr.length) % imgArr.length) {
+        setThumbStartIdx((prev) => (prev - 1 + imgArr.length) % imgArr.length);
+      }
+    }
+  }, [showImgIdx, thumbStartIdx]);
 
   return (
     <div
-      className={styles.container}
+      className={cx('container')}
       style={{ width }}
       {...otherProps}
       ref={ref}
     >
-      <div className={cx('mainImgWrapper')}>
+      <div className={cx('main_img_wrapper')}>
         <button className={cx('dir_btn')} onClick={() => handleImgIdx('left')}>
           <Icon name="ArrowLeft" />
         </button>
         <img
           src={showImg.url}
           alt={showImg.alt}
-          className={cx('mainImg')}
+          className={cx('main_img')}
         ></img>
         <button className={cx('dir_btn')} onClick={() => handleImgIdx('right')}>
           <Icon name="ArrowRight" />
         </button>
       </div>
-      <div className={cx('subImgWrapper')}>
+      <div className={cx('sub_img_wrapper')}>
         {Array.from({
-          length: thumbImgNum,
+          length: thumbImgLength,
         }).map((_, index) => {
-          const srcIdx = (imgIdx + index) % imgArr.length;
+          const srcIdx = (thumbStartIdx + index) % imgArr.length;
 
           return (
             <img
               src={imgArr[srcIdx].url}
               key={index}
-              className={cx('subImg', srcIdx === imgIdx && 'selected')}
+              className={cx('sub_img', srcIdx === showImgIdx && 'selected')}
               onClick={() => handleSubImg(srcIdx)}
             ></img>
           );
