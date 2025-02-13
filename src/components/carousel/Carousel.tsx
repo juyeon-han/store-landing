63bb62ba44d9da90ecb4ee34416b17199ae37e69
한주연 <juyeon.han@yaksonhealthcare.com>
2025-02-13 16:16:02 +0900

    fix: Carousel viewport에 보여질 시 Autoslide 실행

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { EmblaOptionsType } from 'embla-carousel';
 import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
@@ -44,7 +44,6 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
 
     emblaMainApi.on('select', onSelect).on('reInit', onSelect);
   }, [emblaMainApi, onSelect]);
-
   useEffect(() => {
     if (!emblaMainApi) return;
 
@@ -59,10 +58,37 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
       emblaMainApi.plugins()?.autoplay?.play();
     };
 
-    viewport.addEventListener('mouseenter', handleMouseEnter);
-    viewport.addEventListener('mouseleave', handleMouseLeave);
+    // IntersectionObserver 콜백 함수
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        // 영역이 뷰포트에 들어올 시
+        if (entry.isIntersecting) {
+          emblaMainApi.plugins()?.autoplay?.play();
+        } else {
+          emblaMainApi.plugins()?.autoplay?.stop();
+        }
+      });
+    };
 
+    // IntersectionObserver 옵션 설정
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null, // viewport를 기준으로 감지
+      rootMargin: '0px', // 화면에 조금 더 일찍 감지하고 싶다면 마진을 설정 가능
+      threshold: 0.5, // 요소가 50% 이상 보이면 실행
+    });
+
+    // 뷰포트를 감시할 요소를 observer에 등록
+    if (viewport) {
+      observer.observe(viewport);
+    }
+
+    // 마운트 해제 시 observer 종료
     return () => {
+      if (viewport) {
+        observer.unobserve(viewport);
+      }
+
+      // 마우스 이벤트 리스너 제거
       viewport.removeEventListener('mouseenter', handleMouseEnter);
       viewport.removeEventListener('mouseleave', handleMouseLeave);
     };
