43efc2b73ea3973d0e913fc44ca09150c9308a49
한주연 <juyeon.han@yaksonhealthcare.com>
2025-02-18 10:11:52 +0900

    fix: ScrollTab 버튼 추가

--- src/components/tab/scroll-tab/ScrollTab.tsx
+++ src/components/tab/scroll-tab/ScrollTab.tsx
@@ -1,7 +1,9 @@
 import { forwardRef, useCallback } from 'react';
 import classNames from 'classnames';
+import classNameBind from 'classnames/bind';
 import useEmblaCarousel from 'embla-carousel-react';
 import Icon from '@/styles/icons/icons';
+import styles from './ScrollTab.module.scss';
 
 export interface TabsType {
   id: string;
@@ -15,7 +17,8 @@ interface ScrollTabProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const ScrollTab = forwardRef<ScrollTabHandle, ScrollTabProps>((props, ref) => {
-  const { tabs, selectedId, ...otherProps } = props;
+  const cx = classNameBind.bind(styles);
+  const { tabs, selectedId, className, ...otherProps } = props;
   const [emblaRef, emblaApi] = useEmblaCarousel({
     align: 'start',
     dragFree: true,
@@ -33,28 +36,34 @@ const ScrollTab = forwardRef<ScrollTabHandle, ScrollTabProps>((props, ref) => {
   }, [emblaApi]);
 
   return (
-    <section className="embla_tab" {...otherProps} ref={ref}>
-      {/* <button className="embla__tab_button" onClick={onPrevButtonClick}>
+    <section
+      className={cx('tab_container', className)}
+      {...otherProps}
+      ref={ref}
+    >
+      <section className="embla_tab">
+        {/* <button className="embla__tab_button" onClick={onPrevButtonClick}>
         <Icon name="ArrowLeft" size="sm" />
       </button> */}
-      <div className="embla__tab_viewport" ref={emblaRef}>
-        <div className="embla__tab_container">
-          {tabs.map((tab) => (
-            <div className="embla__tab_slide" key={tab.id}>
-              <div
-                className={classNames('embla__tab_slide__content', {
-                  active: tab.id === selectedId,
-                })}
-              >
-                {tab.name}
+        <div className="embla__tab_viewport" ref={emblaRef}>
+          <div className="embla__tab_container">
+            {tabs.map((tab) => (
+              <div className="embla__tab_slide" key={tab.id}>
+                <div
+                  className={classNames('embla__tab_slide__content', {
+                    active: tab.id === selectedId,
+                  })}
+                >
+                  {tab.name}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      <button className="embla__tab_button" onClick={onNextButtonClick}>
-        <Icon name="ArrowRight" size="sm" />
-      </button>
+        <button className="embla__tab_button" onClick={onNextButtonClick}>
+          <Icon name="ArrowRight" size="sm" />
+        </button>
+      </section>
     </section>
   );
 });
