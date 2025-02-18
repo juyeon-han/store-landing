3c144afa23b17b490b6f9048b1622039df8f3b25
한주연 <juyeon.han@yaksonhealthcare.com>
2025-02-18 13:56:32 +0900

    feat: StoreCard 컴포넌트 구현

---
+++ src/components/card/store-card/StoreCard.tsx
@@ -0,0 +1,50 @@
+import { forwardRef } from 'react';
+import classNames from 'classnames/bind';
+import Counter from '@/components/counter/Counter';
+import Icon from '@/styles/icons/icons';
+import { formatNumberWithCommas } from '@/utils/number';
+import styles from './StoreCard.module.scss';
+
+export interface StoreCardType {
+  customers: number;
+  sessions: number;
+  year: number;
+}
+interface StoreCardHandle extends HTMLDivElement {}
+interface StoreCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  data: StoreCardType;
+}
+
+const StoreCard = forwardRef<StoreCardHandle, StoreCardProps>((props, ref) => {
+  const cx = classNames.bind(styles);
+  const { data, className, ...otherProps } = props;
+  return (
+    <div ref={ref} {...otherProps} className={cx('container', className)}>
+      <div className={cx('wrapper')}>
+        <Icon name="Customers" size="xl" />
+        <p>누적 고객 수</p>
+        <p className={cx('data_text')}>
+          + <Counter to={data.customers} /> 명
+        </p>
+      </div>
+      <hr className={cx('divider')} />
+      <div className={cx('wrapper')}>
+        <Icon name="Sessions" size="xl" />
+        <p>누적 관리 수</p>
+        <p className={cx('data_text')}>
+          + <Counter to={data.sessions} /> 회
+        </p>
+      </div>
+      <hr className={cx('divider')} />
+      <div className={cx('wrapper')}>
+        <Icon name="Year" size="xl" />
+        <p>경력</p>
+        <p className={cx('data_text')}>
+          <Counter from={1} to={data.year} /> 년
+        </p>
+      </div>
+    </div>
+  );
+});
+
+export default StoreCard;
