5f5fbb3d0b66e9505fd98fd52acc809cdcd45116
한주연 <juyeon.han@yaksonhealthcare.com>
2025-03-13 17:13:36 +0900

    ref: 페이지 로딩 시 api 불러올 수 있도록 최적화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import { useEffect, useRef } from 'react';
+import classNames from 'classnames/bind';
 import Footer from '@/components/footer/Footer';
 import Gnb, { GnbMenusType } from '@/components/nav/gnb/Gnb';
+import { useIntersectionObserver } from '@/hooks/useInteractionObserver';
 import CarePage from '@/pages/care';
 import PlacePage from '@/pages/place';
 import PromotionPage from '@/pages/promotion';
 import QnaPage from '@/pages/qna';
 import ReviewPage from '@/pages/review';
-
+import Icon from '@/styles/icons/icons';
+import styles from './index.module.scss';
 function Home() {
+  const cx = classNames.bind(styles);
   const menus: GnbMenusType[] = [
     { id: 'intro', name: '지점 & 원장님 소개' },
     { id: 'review', name: '리뷰' },
@@ -15,14 +20,58 @@ function Home() {
     { id: 'qna', name: '문의사항' },
   ];
 
+  const pageRefs = useRef<Array<HTMLDivElement | null>>([]);
+  const { setElements, isVisible } = useIntersectionObserver({
+    threshold: 0,
+    rootMargin: '100px',
+  });
+
+  const pageList = [
+    {
+      id: 'place',
+      component: <PlacePage />,
+    },
+    {
+      id: 'review',
+      component: <ReviewPage />,
+    },
+    {
+      id: 'promotion',
+      component: <PromotionPage />,
+    },
+    {
+      id: 'care',
+      component: <CarePage />,
+    },
+    {
+      id: 'qna',
+      component: <QnaPage />,
+    },
+  ];
+
+  useEffect(() => {
+    setElements(pageRefs.current);
+  }, []);
+
   return (
     <div>
       <Gnb menus={menus} />
-      <PlacePage />
-      <ReviewPage />
-      <PromotionPage />
-      <CarePage />
-      <QnaPage />
+      {pageList.map((page, index) => (
+        <div ref={(el) => (pageRefs.current[index] = el)} key={page.id}>
+          {isVisible[index] ? (
+            page.component
+          ) : (
+            <div className={cx('page')}>
+              <Icon
+                name="Refresh"
+                size={40}
+                color="brand300"
+                className={cx('icon')}
+              />
+            </div>
+          )}
+        </div>
+      ))}
       <Footer />
     </div>
   );
