039e1ec31b2d1aad1461ba1773623d5807802c9f
한주연 <juyeon.han@yaksonhealthcare.com>
2025-03-14 16:43:50 +0900

    chore: promotion 이미지 tempUrl 대체 및 payCard 높이 재설정

--- src/pages/promotion/index.tsx
+++ src/pages/promotion/index.tsx
@@ -17,8 +17,6 @@ const PromotionPage = () => {
   const promotionRefs = useRef<(HTMLDivElement | null)[]>([]);
   const { setElements, isVisible } = useIntersectionObserver();
   const { pageParams } = usePageParams();
-  const tempUrl =
-    'https://images.unsplash.com/photo-1737914111975-b4d513d783e0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D';
 
   const {
     data: promotionData,
@@ -69,9 +67,7 @@ const PromotionPage = () => {
                 key={index}
                 condition={item.pagePromotionTitle}
                 product={item.pagePromotionSubtitle}
-                imgUrl={
-                  tempUrl ?? item.pagePromotionImage[0].pagePromotionImageUrl
-                }
+                imgUrl={item.pagePromotionImage[0].pagePromotionImageUrl}
                 num={Number(item.pagePromotionIdx)}
                 event_date={formatDateRange(
                   item.pagePromotionStime,
