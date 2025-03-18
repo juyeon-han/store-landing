7f13d22edc28625873727ee7adecd7d6fa056371
한주연 <juyeon.han@yaksonhealthcare.com>
2025-03-18 10:36:55 +0900

    feat: footer data 매핑

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,10 @@
 import { forwardRef } from 'react';
 import classNames from 'classnames/bind';
-import logo from '@/assets/images/yakson_logo.png';
+import { useGetPageDetail } from '@/api/service/page';
+import AsyncContent from '@/components/asyncContent/AsyncContent';
+import ErrorFallback from '@/components/fallback/ErrorFallback';
+import FooterSkeleton from '@/components/skeleton/footer/FooterSkeleton';
+import { usePageParams } from '@/hooks/usePageParams';
 import styles from './Footer.module.scss';
 
 interface FooterHandle extends HTMLDivElement {}
@@ -48,19 +52,44 @@ interface FooterProps extends React.HTMLAttributes<HTMLDivElement> {}
 // ];
 const Footer = forwardRef<FooterHandle, FooterProps>((props, ref) => {
   const cx = classNames.bind(styles);
+  const { pageParams } = usePageParams();
+
+  const {
+    data: footerData,
+    isPending,
+    isError,
+    error,
+    refetch,
+  } = useGetPageDetail({
+    pageNum: pageParams.pageNum,
+    options: {
+      throwOnError: false,
+    },
+  });
+
   return (
     <footer className={cx('container')} {...props} ref={ref}>
-      <img className={cx('logo')} src={logo} alt="logo"></img>
-      <div className={cx('wrapper')}>
-        <div className={cx('footer_top')}>
-          <div>
-            <span className={cx('title')}>상담전화</span>
-            <span className={cx('text')}>
-              02-469-7007 (평일: 오전 10시 ~ 오후 10시, 토: 오전 9시 ~ 오후 5시
-              30분, 일: 정기휴무)
-            </span>
-          </div>
-          {/* <div className={cx('link')}>
+      <AsyncContent
+        isError={isError}
+        isPending={isPending}
+        ErrorComponent={
+          <ErrorFallback error={error} resetErrorBoundary={refetch} />
+        }
+        PendingComponent={<FooterSkeleton />}
+      >
+        {footerData ? (
+          <>
+            <span className={cx('branch')}>{footerData?.pageBranchName}</span>
+            <div className={cx('wrapper')}>
+              <div className={cx('footer_top')}>
+                <div>
+                  <span className={cx('title')}>상담전화</span>
+                  <span className={cx('text')}>
+                    {footerData?.pageBranchTel} /{' '}
+                    {footerData?.pageBranchOperationHours}
+                  </span>
+                </div>
+                {/* <div className={cx('link')}>
             {text_link.map((text) => (
               <a
                 key={text.id}
@@ -72,26 +101,30 @@ const Footer = forwardRef<FooterHandle, FooterProps>((props, ref) => {
               </a>
             ))}
           </div> */}
-        </div>
-        <hr />
-        <div className={cx('footer_info')}>
-          <div>
-            <div className={cx('info_item')}>
-              <span className={cx('title')}>대표</span>
-              <span className={cx('text')}>정진연</span>
-            </div>
-            <div className={cx('info_item')}>
-              <span className={cx('title')}>개인정보관리 책임자</span>
-              <span className={cx('text')}>정진연</span>
-            </div>
-            <div className={cx('info_item')}>
-              <span className={cx('title')}>주소</span>
-              <span className={cx('text')}>
-                서울 광진구 아차산로 229 한림타워 4층 405호
-              </span>
-            </div>
-          </div>
-          {/* <div>
+              </div>
+              <hr />
+              <div className={cx('footer_info')}>
+                <div>
+                  <div className={cx('info_item')}>
+                    <span className={cx('title')}>대표</span>
+                    <span className={cx('text')}>
+                      {footerData?.pageOwnerName}
+                    </span>
+                  </div>
+                  <div className={cx('info_item')}>
+                    <span className={cx('title')}>개인정보관리 책임자</span>
+                    <span className={cx('text')}>
+                      {footerData?.pageOwnerName}
+                    </span>
+                  </div>
+                  <div className={cx('info_item')}>
+                    <span className={cx('title')}>주소</span>
+                    <span className={cx('text')}>
+                      {footerData?.pageBranchAddr}
+                    </span>
+                  </div>
+                </div>
+                {/* <div>
             <div className={cx('info_item')}>
               <span className={cx('title')}>사업자등록번호</span>
               <span className={cx('text')}>211-88-34433</span>
@@ -108,12 +141,12 @@ const Footer = forwardRef<FooterHandle, FooterProps>((props, ref) => {
           <div>
             <span className={cx('text')}>개인정보취급방침</span>
           </div> */}
-        </div>
-        <div className={cx('footer_bottom')}>
-          <span className={cx('copyright')}>
-            COPYRIGHT 2025. YAKSONHEALTHCARE ALL RIGHT RESERVED
-          </span>
-          {/* <div className={cx('icon_btn_wrapper')}>
+              </div>
+              <div className={cx('footer_bottom')}>
+                <span className={cx('copyright')}>
+                  COPYRIGHT 2025. YAKSONHEALTHCARE ALL RIGHT RESERVED
+                </span>
+                {/* <div className={cx('icon_btn_wrapper')}>
             {icons_link.map((icon) => (
               <a
                 key={icon.id}
@@ -126,8 +159,13 @@ const Footer = forwardRef<FooterHandle, FooterProps>((props, ref) => {
               </a>
             ))}
           </div> */}
-        </div>
-      </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </AsyncContent>
     </footer>
   );
 });
